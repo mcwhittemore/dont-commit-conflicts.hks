677cb0d6e87a1929f9c3057182474b7357421a4e
Matthew Chase Whittemore <matthew@socialtables.com>
2013-10-17 01:29:24 -0400

    refactor to use sgf

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var colors = require("colors");
-var exec = require("child_process").exec;
-var fs = require("fs");
+var sgf = require("staged-git-files");
+sgf.includeContent = true;
 var isConflict = require("./lib/is-conflict");
 
 var hook = process.argv[2];
@@ -8,36 +8,19 @@ var hook = process.argv[2];
 if (hook != "pre-commit") {
     console.error("This is only going to work with pre-commit");
 } else {
-    //TODO: it would be sweet if hooks had an api that gave me these... 
-    exec("git diff --cached --name-status HEAD --diff-filter=AMR", function(err, stdout, stderr) {
+    sgf("AMR", function(err, results) {
         if (err) {
             console.log("GIT ERROR".red);
             console.log(err);
             process.exit(1);
-        } else if (stderr) {
-            console.log(stderr);
-            process.exit(1);
+        } else if (results.length == 0) {
+            console.log("> No files to commit".yellow);
         } else {
-            var lines = stdout.split("\n");
-            var iLines = lines.length;
             var bad_files = [];
-            while (iLines--) {
-                var line = lines[iLines];
-                var filename = line.split("\t")[1];
-                if (line != "") {
-                    try {
-                        var content = fs.readFileSync(filename, {
-                            encoding: "utf8"
-                        });
-                    } catch (err) {
-                        console.log(filename + ": ERROR".red);
-                        console.log(err.message);
-                        process.exit(1);
-                    }
-
-                    if (isConflict(content)) {
-                        bad_files.push(filename);
-                    }
+            var iFiles = results.length;
+            while (iFiles--) {
+                if (isConflict(results[iFiles].content)) {
+                    bad_files.push(results[iFiles].filename);
                 }
             }
 
